0cafeecddc7b5f6cb2cc49584e0695d5810de7f6
Adam Byrne <adamrbyrne@gmail.com>
2024-02-10 17:31:15 +0100

    LP ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
 import Link from "next/link";
-
+import Image from "next/image";
 import { Send, Network, Zap, CircuitBoard } from "lucide-react";
 
 export default function Component() {
@@ -34,7 +34,13 @@ export default function Component() {
 				<section className="w-full py-6 sm:py-12 md:py-24 lg:py-32 xl:py-48">
 					<div className="container px-4 md:px-6">
 						<div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
-							<div className="bg-neutral-100 dark:bg-neutral-800 mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square" />
+							<Image
+								src="/shuttle.svg"
+								alt="Space shuttle wireframe"
+								width={600}
+								height={600}
+								className="mx-auto aspect-video overflow-hidden rounded-xl object-fill sm:w-full lg:order-last lg:aspect-square"
+							/>
 							<div className="flex flex-col justify-center space-y-4">
 								<div className="space-y-2">
 									<h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
