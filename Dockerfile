cec39192812f6ab01dd2ea46b42692a7d06261f3
Adam Byrne <adamrbyrne@gmail.com>
2024-03-18 00:48:16 +0000

    Update configuration files

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
-FROM node:20-alpine
-WORKDIR /app
-COPY package*.json ./
-RUN npm install --legacy-peer-deps
+FROM node:18-alpine3.18
+RUN adduser -D agent
+WORKDIR /
 COPY . .
+
+COPY package*.json ./
+RUN npm install
+
 RUN npm run build
 EXPOSE 3000
+
+USER agent
 CMD ["npm", "start"]
--- sst.config.ts
+++ sst.config.ts
@@ -1,18 +1,27 @@
+import { Distribution } from "aws-cdk-lib/aws-cloudfront";
+import { Architecture } from "aws-cdk-lib/aws-lambda";
 import { SSTConfig } from "sst";
-import { Bucket, NextjsSite } from "sst/constructs";
+import { App, Bucket, NextjsSite, Stack } from "sst/constructs";
 
 export default {
-	config(_input: any) {
+	config() {
 		return {
 			name: "soar",
 			region: "eu-west-1",
 		};
 	},
-	stacks(app: any) {
-		app.stack(function Site({ stack }: { stack: any }) {
+	async stacks(app: App) {
+		app.stack(function Site({ stack }: { stack: Stack }) {
 			const bucket = new Bucket(stack, "public");
+
 			const site = new NextjsSite(stack, "site", {
+				permissions: [bucket],
 				bind: [bucket],
+				cdk: {
+					server: { architecture: Architecture.X86_64 },
+				},
+				runtime: "nodejs20.x",
+				memorySize: 4096,
 			});
 
 			stack.addOutputs({
